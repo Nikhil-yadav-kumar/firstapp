f5971fb0530835419cd6e88602b65076b18c0d57
Nikhil-yadav-kumar <nikhilyadav72646@gmail.com>
2026-06-16 21:38:38 +0530

    1st

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Item from './components/Item';
 
 function App() {
   return (
-    <div className="App">
-     Hello Nikhil
+    <div>
+      <div className="App">Hello Nikhil</div>
+    <Item></Item>
     </div>
+
   );
 }
 
